797c1382346be2f2371af98a205cf5a29acc9ceb
eratiani <62502052+eratiani@users.noreply.github.com>
2022-04-14 13:25:12 +0400

    docs:
    upgrade code

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
 let x = document.querySelector(".burgerList2")
+let z = document.querySelector(".burger")
+x.addEventListener("click", burger);
+z.addEventListener("click", burger);
 function burger() {
-    if (x.style.transform === "translateY(-500%)") {
+    if (x.style.transform === "translateY(-1500%)") {
         x.style.transform = "translateY(0)";
-        console.log("1");
-      
-    } else {
-    
-      x.style.transform = "translateY(-500%)";
-      
-    }
-}
-function dissapear() {
     
 
-        x.style.transform = "translateY(-500%)";
-   
-  }
+    } else {
+
+        x.style.transform = "translateY(-1500%)";
+
+    }
+}
+
+function dissapear() {
+
+
+    x.style.transform = "translateY(-1500%)";
+
+}
