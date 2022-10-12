be89e56d0910aa302070cb2fe40828d24224e87c
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2022-10-12 10:34:12 +0400

    feat: implement smooth scroll

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let x = document.querySelector(".burgerList2")
+let y = document.querySelector(".burgerList")
 let z = document.querySelector(".burger")
 x.addEventListener("click", burger);
 z.addEventListener("click", burger);
@@ -19,4 +20,40 @@ function dissapear() {
 
     x.style.transform = "translateY(-1500%)";
 
+}
+const smoothScroll = (element)=>{
+    const el = element.getBoundingClientRect();
+    element.scrollIntoView({
+        behavior: "smooth"
+      });
+}
+y.addEventListener("click", (e)=>{
+    e.preventDefault();
+    smoothScrollTar(e)   
+
+});
+x.addEventListener("click", (e)=>{
+    e.preventDefault();
+    smoothScrollTar(e)
+})
+function smoothScrollTar(e) {
+    if (Number(e.target.id) === 1) {
+        const targ = document.getElementById("header")
+        smoothScroll(targ);
+    } else if(Number(e.target.id) === 2) {
+        const targ = document.getElementById("mySkills")
+        return smoothScroll(targ);
+    } else if(Number(e.target.id)===3) {
+        const targ = document.getElementById("codeExample")
+        return smoothScroll(targ);
+    } else if(Number(e.target.id)===4) {
+        const targ = document.getElementById("experience")
+        smoothScroll(targ);
+    } else if(Number(e.target.id)===5) {
+        const targ = document.getElementById("contact")
+        smoothScroll(targ);
+    } else if(Number(e.target.id)===6) {
+        const target = document.getElementById("other")
+        smoothScroll(target);
+    } 
 }
